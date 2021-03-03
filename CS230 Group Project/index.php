4e4344d57979c13d04e1c26ab728a035969563a6
test <test@gmail.com>
2021-03-03 13:41:14 -0500

    Change Test

--- CS230 Group Project/index.php
+++ CS230 Group Project/index.php
@@ -2,68 +2,10 @@
 require 'includes/header.php';
 ?>
 
-
 <main>
-    <link rel="stylesheet" href="css/about.css">
-
-    <div class="about" >
-        <h1 style= "font-size: 48px" >Lightsabers - About Us</h1>
-        <p>Hello, Welcome to Lightsabers.com! This is our team! We are the official galactic producers and distributors 
-        of lightsaber blades and hilts. order yours today!</p>
-
-    </div>
-
-    <div class="row">
-        <div class="column">
-            <div class="card">
-                <img src="images/luke.jpeg">
-                <div class="container">
-                    <h2 style= "text-decoration: underline 2px black">CEO</h2>
-                    <p>Luke Skywalker</p>
-                    <p style= "font-size: 12px">Email: LSkywalker@lightsabers.com</p>
-                </div>
-
-            </div>
-        </div>
-
-        <div class="column">
-            <div class="card">
-                <img src="images/obiwan.jpeg">
-                <div class="container">
-                    <h2 style= "text-decoration: 2px underline black" >CFO</h2>
-                    <p>Obi-Wan-Kenobi</p>
-                    <p style= "font-size: 12px">Email: HelloThere@lightsabers.com</p>
-                </div>
-
-            </div>
-        </div>
-    
-        <div class="column">
-            <div class="card">
-                <img src="images/Anakin.webp">
-                <div class="container">
-                    <h2 style= "text-decoration: underline 2px black">Website Manager</h2>
-                    <p>Anakin Skywalker</p>
-                    <p style= "font-size: 12px">Email: MasterSkywalker@lightsabers.com</p>
-                </div>
-
-            </div>
-        </div>
-
-        <div class="column">
-            <div class="card">
-                <img src="images/yoda.jpeg">
-                <div class="container">
-                    <h2 style= "text-decoration: underline 2px black" >Founder</h2>
-                    <p>Yoda</p>
-                    <p style= "font-size: 12px">Email: Yoda@lightsabers.com</p>
-                </div>
-
-            </div>
-        </div>
 
         <div class="footer">
-    <footer>Copyright Lightsabers Inc</footer>
+    <footer>Copyright RecipeDaddy Inc</footer>
 
     </div>
 
