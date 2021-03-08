601d3b5589457f816ca7af72218318ad2f2f9039
mlemon0037 <mal0037@mix.wvu.edu>
2021-03-08 15:48:16 -0500

    commented out login carousel + fix recipe format

--- CS230 Group Project/test-recipe.php
+++ CS230 Group Project/test-recipe.php
@@ -8,19 +8,21 @@ require 'includes/dbhandler.php';
 $rec = "SELECT * FROM recipes WHERE rid=(SELECT max(rid) FROM recipes)";
 $res = mysqli_query($conn, $rec);
 $row = mysqli_fetch_array($res);
-$exp = ';';
 
 $ingarr = preg_split("/;/", $row['ingredients']); 
 $stparr = preg_split("/;/", $row['steps']); 
+$creator = $row['creator'];
 
 $name = $row['name'];
-echo "<h2>$name</h2>";
+echo "<h2 style='text-align:center'>$name</h2>";
 
-echo "<h3>Ingredients: </h3>";
+echo "<h3 style='text-align:center'>Ingredients: </h3>";
 foreach ($ingarr as $ingvalue) {
-    echo "<p>$ingvalue</p>";
+    echo "<p style='text-align:center'>$ingvalue</p>";
 }
-echo "<h3>Steps: </h3>";
+echo "<h3 style='text-align:center'>Steps: </h3>";
 foreach ($stparr as $stpvalue) {
-    echo "<p>$stpvalue</p>";
+    echo "<p style='text-align:center'>$stpvalue</p>";
 }
+
+echo "<p style='text-align:center'>Recipe added by: $creator</p>";
