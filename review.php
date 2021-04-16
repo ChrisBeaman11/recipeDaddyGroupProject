9d53388bf9335b722144435368b999e53c71938b
zmh0007 <zmh0007@mix.wvu.edu>
2021-04-16 13:46:38 -0400

    Fixes to Reviews Page

--- review.php
+++ review.php
@@ -7,46 +7,76 @@ require 'includes/review-helper.php';
 <main>
     <span id="testAvg"></span>
 
-    <div class="container" align="center" style="max-width: 800px;">
-
+    <div class="container" align='center' style='max-width: 800 px;'>
         <div class="my-auto">
 
-            <form id="review-form" action="includes/review-helper.php" method="post">
+            <div class="card">
+                <?php
+                $id = $_GET['id'];
+                $sql = 'SELECT * FROM recipes WHERE rid='.$id.'';
+                $query = mysqli_query($conn, $sql);
+                $row = mysqli_fetch_assoc($query);
+                echo '<div class = "card">
+                    <img src="gallery/'.$row["pic"].'">
+                    <h3>'.$row["name"].'</h3>
+                    <p>'.$row["ingredients"].'</p>
+                    <p>'.$row["steps"].'</p>
+                </a>
+                </div>';
+                ?>
+            </div>
 
-                <div class="container">
+            <form id="favorites-button" action="includes/favorites-helper.php" method="get">
 
-                    <i class="fa fa-star fa-2x star-rev" data-index="1"></i>
-                    <i class="fa fa-star fa-2x star-rev" data-index="2"></i>
-                    <i class="fa fa-star fa-2x star-rev" data-index="3"></i>
-                    <i class="fa fa-star fa-2x star-rev" data-index="4"></i>
-                    <i class="fa fa-star fa-2x star-rev" data-index="5"></i>
-
+                <input type="hidden" name="rid" id="rid" value=<?php echo $_GET['id'];?>></input>
+                <div class="form-group">
+                    <button class="btn btn-outline-primary" type="submit" name="favorites-submit" id="favorites-submit"
+                        style="width: 100%">Favorite</button>
                 </div>
-
-                <div class="form-group" style="margin-top: 15px;"></div>
-                <label class="title-label" for="review-title" style="font-size: 16px; font-weight: bold;">Title</label>
-                <input type="text" name="review-title" id="review-title" style="width: 100%; margin-bottom: 10px;">
-                <textarea name="review" id="review-text" cols="50" rows="3" placeholder="Enter a comment..."></textarea>
-
-                <input type="hidden" name="rating" id="rating">
-                <input type="hidden" name="item_id" value="<?php echo $_GET['id'];?>">
-
-
-
-        </div>
-
-        <div class="form-group">
-            <button class="btn btn-outline-danger" type="submit" name="review-submit" id="review-submit"
-                style="width: 100%">Review</button>
 
             </form>
 
-        </div>
+
+            <div class="container" align="center" style="max-width: 800px;">
+
+                <div class="my-auto">
+
+                    <form id="review-form" action="includes/review-helper.php" method="post">
+
+                        <div class="container">
+
+                            <i class="fa fa-star fa-2x star-rev" data-index="1"></i>
+                            <i class="fa fa-star fa-2x star-rev" data-index="2"></i>
+                            <i class="fa fa-star fa-2x star-rev" data-index="3"></i>
+                            <i class="fa fa-star fa-2x star-rev" data-index="4"></i>
+                            <i class="fa fa-star fa-2x star-rev" data-index="5"></i>
+
+                        </div>
+
+                </div>
+                <div class="form-group" style="margin-top: 15px;">
+                    <label class="title-label" for="reveiw-title"
+                        style="font-size:16px; font-weight: bold;">Title</label>
+                    <input type="text" name="review-title" id="review-title" style="width: 100px; margin-bottom: 10px;">
+                    <textarea name="review" id="review-text" cols="90" rows="3"
+                        placeholder="Enter a comment..."></textarea>
+
+                    <input type="hidden" name="rating" id="rating">
+                    <input type="hidden" name="item_id" value="<?php echo $_GET['id'];?>">
+                </div>
+
+                <div class="form-group">
+                    <button class="btn btn-outline-danger" type="submit" name="review-submit" id="review-submit"
+                        style="width: 100%">Review</button>
+
+                    </form>
+
+                </div>
 
 
-    </div>
+            </div>
 
-    <span id="review_list"></span>
+            <span id="review_list"></span>
 </main>
 
 <script type="text/javascript">
