a83d2bb83ca1cc63a12db3445fc25723e3492115
karlscheib <kes0049@mix.wvu.edu>
2021-04-22 16:56:11 -0400

    Fixed uploading profile pic (Updated database)

--- includes/upload-helper.php
+++ includes/upload-helper.php
@@ -33,12 +33,12 @@ if (isset($_POST['prof-submit'])){
         exit();
     } else {
         $new_name = uniqid('', true).".".$ext;
-        $destination = '../profiles/'.$new_name;
+        $destination = "profiles/".$new_name;
 
         $sql = "UPDATE profiles SET profpic='$destination' WHERE uname='$uname'";
         mysqli_query($conn, $sql);
 
-        move_uploaded_file($file_tmp_name, $destination);
+        move_uploaded_file($file_tmp_name, "../".$destination);
         header("Location: ../profile.php?success=Uploaded");
         exit();
     }
