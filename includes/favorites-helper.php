d8f74519c3d1d97dfcf3d9783e13d77729495831
mlemon0037 <mal0037@mix.wvu.edu>
2021-04-26 16:36:35 -0400

    fixed duplicate favorites additions

--- includes/favorites-helper.php
+++ includes/favorites-helper.php
@@ -8,8 +8,14 @@
         $userid = $_SESSION['uid'];
         $recipeid = $_GET['rid'];
         
-        $sql = "INSERT INTO favorites (pid, rid) VALUES('$userid', '$recipeid')";
-        mysqli_query($conn, $sql);
+        $sql1 = "SELECT * FROM favorites WHERE pid = '$userid' AND rid = '$recipeid';";
+        $query = mysqli_query($conn, $sql1);
+        $row = mysqli_fetch_array($query);
+
+        if ($row['favid'] == NULL){
+            $sql = "INSERT INTO favorites (pid, rid) VALUES('$userid', '$recipeid');";
+            mysqli_query($conn, $sql);
+        }
     }
     header("Location: ../review.php?id=$recipeid");
     exit();
